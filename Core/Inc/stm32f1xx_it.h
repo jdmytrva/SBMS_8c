97aaaa989a3619580e7a92968ad0dfc23fa71ead
Vasyld(Work) <vdmytruk@gmail.com>
2021-03-11 18:45:28 +0200

    not work

--- Core/Inc/stm32f1xx_it.h
+++ Core/Inc/stm32f1xx_it.h
@@ -58,7 +58,7 @@ void PendSV_Handler(void);
 void SysTick_Handler(void);
 void DMA1_Channel1_IRQHandler(void);
 void USART1_IRQHandler(void);
-void USART2_IRQHandler(void);
+void USART3_IRQHandler(void);
 void TIM7_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
